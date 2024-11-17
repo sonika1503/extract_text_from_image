a86a3080e1f5adf936a93a224c8ef75eb48d5f33
Sonika Choudhury <151528657+sonika1503@users.noreply.github.com>
2024-11-18 01:05:09 +0530

    Update config.py

--- api/config.py
+++ api/config.py
@@ -3,5 +3,10 @@ import os
 MONGODB_URL = os.getenv("MONGODB_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-with open('../label_prompt.txt', 'r') as file:
-    LABEL_READER_PROMPT = file.read()
+LABEL_READER_PROMPT = None
+
+def load_label_reader_prompt():
+    global LABEL_READER_PROMPT
+    if LABEL_READER_PROMPT is None:
+        with open('../label_prompt.txt', 'r') as file:
+            LABEL_READER_PROMPT = file.read()
